9c93c2f79c85523ad567aa70378b609939579929
mxhectorvega <mxhectorvega@gmail.com>
2024-01-28 15:20:01 -0600

    update

---
+++ minetest-server-2024-01-28/games/minetest/mods/mobs/lucky_block.lua
@@ -0,0 +1,15 @@
+
+lucky_block:add_blocks({
+	{"dro", {"mobs:meat_raw"}, 5},
+	{"dro", {"mobs:meat"}, 5},
+	{"dro", {"mobs:nametag"}, 1},
+	{"dro", {"mobs:leather"}, 5},
+	{"dro", {"default:stick"}, 10},
+	{"dro", {"mobs:net"}, 1},
+	{"dro", {"mobs:lasso"}, 1},
+	{"dro", {"mobs:shears"}, 1},
+	{"dro", {"mobs:protector"}, 1},
+	{"dro", {"mobs:fence_wood"}, 10},
+	{"dro", {"mobs:fence_top"}, 12},
+	{"lig"}
+})
